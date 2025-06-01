0c4992625960c15df8b9001ea8c152aeecb563a1
aveluth <65974995+aveluth@users.noreply.github.com>
2025-06-01 16:40:49 +0000

    Deploying to gh-pages from @ nlpsi-workshop/nlpsi-workshop.github.io@070ae9ca59becaee267c0db05b6e0864cab082ba 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,8 +16,8 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/call_for_papers/";
           },
-        },{id: "nav-️-event-schedule",
-          title: "🗓️ Event Schedule",
+        },{id: "nav-schedule",
+          title: "Schedule",
           description: "",
           section: "Navigation",
           handler: () => {
